5cf5757ecf8077537229df917bfb1e8f2a7e6721
Clayton S <desertcow07@gmail.com>
2023-06-11 12:39:33 -0700

    Progress on header flexbox, EOS

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -11,31 +11,123 @@ const Header = () => {
 
   return (
 
-    <div className="HeaderClass mt-auto mb-0">
+    <div className="HeaderClass mt-auto mb-0 mr-5">
 
-      <div className="d-flex align-items-center justify-content-left">
-        <div className="text-center">
+      {/* <div className="d-flex align-items-center justify-content-left"> */}
+        {/* <div className="text-center">
           <img src={require("../../img/BC_Logo_Clear_1.png")}
             className="homePageLogo"
             alt="Board Club Logo" />
-        </div>
+        </div> */}
 
-        <Container className="m-5">
-          <div className="d-flex flex-col">
-            <div className="d-flex flex-row">
+        <div class="container">
+          <div class="row">
+            <div class="col">
+              <div className="text-center">
+                <img src={require("../../img/BC_Logo_Clear_1.png")}
+                  className="homePageLogo"
+                  alt="Board Club Logo" />
+              </div>
+            </div>
+            <div class="col d-flex align-items-center">
+                 {/* SPACER BOX! */}
+            </div>
+            <div class="col d-flex align-items-center">
+                 {/* SPACER BOX! */}
+            </div>
+            <div class="col d-flex align-items-center">
+                <div class="row">
+                  <div class="col headerTideIcon pt-0 mt-3">
+                    <div className="text-center">
+                      <img src={require("../../img/tide_icon.png")}
+                        className="headerTideIcon"
+                      alt="Tide Icon" />
+                    </div>
+                  </div>
+                  <div class="col headerCurrentTide text-center pt-2">
+                    0.4 ft
+                  </div>
+                  <div class="col headerTideArrow pt-0">
+                     <MaterialSymbol icon="arrow_upward" size={60} fill grade={-25} color='black' />
+                  </div>
+                </div>
+            </div>
+            <div class="col d-flex align-items-center">
+              <div class="row">
+                <div class="col">
+                  Column
+                </div>
+                <div class="col">
+                  Column
+                </div>
+                <div class="w-100"></div>
+                <div class="col">
+                  Column
+                </div>
+                <div class="col">
+                  Column
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* <div class="row">
+            <div class="col">
+              1 of 3
+            </div>
+            <div class="col">
+              2 of 3
+            </div>
+            <div class="col">
+              3 of 3
+            </div>
+          </div> */}
+        </div>
+          {/* <div class="d-flex flex-column justify-content-end">
               <div className="text-center">
                 <img src={require("../../img/tide_icon.png")}
                   className="headerTideIcon"
                   alt="Tide Icon" />
               </div>
+            <div>
+              <p className="m-2 headerTideText text-center">3.4 ft</p>
             </div>
-            <div className="d-flex flex-row">
-              <MaterialSymbol icon="settings" size={60} fill grade={-25} color='black' />
+            <div className="text-center">
+                <MaterialSymbol icon="arrow_upward" size={60} fill grade={-25} color='black' />
             </div>
-          </div>
 
-        </Container>
-      </div>
+            <div class='some-page-wrapper'>
+              <div class='flex-row'>
+                <div class='flex-column'>
+                  <div class='blue-column'>
+                    Some Text in Column One
+                  </div>
+                </div>
+                <div class='column'>
+                  <div class='green-column'>
+                    Some Text in Column Two
+                  </div>
+                </div>
+              </div>
+            </div> */}
+
+          {/* <div class="d-flex flex-column justify-content-end">
+              <div className="text-center">
+                <img src={require("../../img/tide_icon.png")}
+                  className="headerTideIcon"
+                  alt="Tide Icon" />
+              </div>
+            <div>
+              <p className="m-2 headerTideText">3.4 ft</p>
+            </div>
+            <div className="">
+                <MaterialSymbol icon="arrow_upward" size={60} fill grade={-25} color='black' />
+            </div>
+          </div> */}
+          {/* </div> */}
+
+
+        {/* </Container> */}
+      {/* </div> */}
     </div>
   );
 };
