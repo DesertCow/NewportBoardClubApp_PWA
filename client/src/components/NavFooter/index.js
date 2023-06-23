8143b513ed0816114f061c00f05f9659d6abcbd4
Clayton S <desertcow07@gmail.com>
2023-06-23 13:29:18 -0700

    Polished Rentals/About pages

--- client/src/components/NavFooter/index.js
+++ client/src/components/NavFooter/index.js
@@ -16,17 +16,17 @@ const NavFooter = () => {
       <Navbar className="d-flex FooterClass" fixed="bottom" expand="lg">
         <Container className="p-0 justify-content-center">
           <Nav className="d-flex flex-row p-0">
-            <Nav.Link className="col mx-5 px-3" href="/">
+            <Nav.Link className="col mx-5 px-2" href="/">
               <div className="text-center">
                 <MaterialSymbol icon="house" size={60} fill grade={-25} color='black' />
               </div>
             </Nav.Link>
-            <Nav.Link className="col mx-5 px-3" href="/surf_log">
+            <Nav.Link className="col mx-5 px-2" href="/surf_log">
               <div className="text-center">
                 <MaterialSymbol icon="import_contacts" size={60} fill grade={-25} color='black' />
               </div>
             </Nav.Link>
-            <Nav.Link className="col mx-5 px-3" href="/user_settings">
+            <Nav.Link className="col mx-5 px-2" href="/user_settings">
               <div className="text-center">
                 <MaterialSymbol icon="settings" size={60} fill grade={-25} color='black' />
               </div>
