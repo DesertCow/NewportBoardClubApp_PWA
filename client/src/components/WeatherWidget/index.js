7b6977871363ec046b7d743597c93690513a949e
Clayton S <desertcow07@gmail.com>
2023-06-24 12:03:08 -0700

    Flushed out part of weather widget HTML

--- client/src/components/WeatherWidget/index.js
+++ client/src/components/WeatherWidget/index.js
@@ -2,7 +2,7 @@
 // import Navbar from 'react-bootstrap/Navbar';
 // import Nav from 'react-bootstrap/Nav';
 
-// import { MaterialSymbol } from 'react-material-symbols';
+import { MaterialSymbol } from 'react-material-symbols';
 
 
 
@@ -19,14 +19,91 @@ const WeatherWidget = () => {
             <button type="button" className="close" data-dismiss="modal" aria-label="Close">
               <span aria-hidden="true">&times;</span>
             </button>
-            <div class="d-flex w-100 justify-content-around">
-              <div class="col-3">
-                1 piece
-              </div>
-              <div class="col-6">
-                2 pieces piece
+            <div className="d-flex w-100 justify-content-around">
+              <div className="d-flex col-8">
+                <div className="row-3">
+                </div>
+                  <div className="d-flex align-items-center justify-content-center row-3 px-3">
+                  <MaterialSymbol icon="waves" size={50} fill grade={-25} color='black' />
+                  </div>
+                  <div className="row-3 px-3 weatherWidgetTempFont d-flex align-items-center justify-content-center">
+                    {/* {liveAirTemp} &deg;F */}
+                    64 &deg;F
+                  </div>
+                </div>
+              <div className="col-6 d-flex align-items-center justify-content-center">
+                <MaterialSymbol icon="waves" size={50} fill grade={-25} color='black' />
               </div>
             </div>
+            <div className="d-flex w-100 justify-content-around mt-3">
+              <div className="d-flex col-8">
+                <div className="row-3">
+                </div>
+                  <div className="d-flex align-items-center justify-content-center row-3 px-3">
+                  <MaterialSymbol icon="sunny" size={50} fill grade={-25} color='black' />
+                  </div>
+                  <div className="row-3 px-3 weatherWidgetTempFont d-flex align-items-center justify-content-center">
+                    {/* {liveAirTemp} &deg;F */}
+                    72 &deg;F
+                  </div>
+                </div>
+              <div className="col-6">
+                Blackies: 
+              </div>
+            </div>
+            <div className="d-flex w-100 justify-content-around mt-3">
+              <div className="d-flex col-8">
+                <div className="row-3">
+                </div>
+                  <div className="d-flex align-items-center justify-content-center row-3 px-3">
+                  <MaterialSymbol icon="air" size={50} fill grade={-25} color='black' />
+                  </div>
+                  <div className="row-3 px-3 weatherWidgetTempFont d-flex align-items-center justify-content-center">
+                    {/* {liveAirTemp} &deg;F */}
+                    12 mph
+                  </div>
+                </div>
+              <div className="col-6">
+                36th Street:
+              </div>
+            </div>
+            <div className="d-flex w-100 justify-content-around mt-3">
+              <div className="d-flex col-8">
+                <div className="row-3">
+                </div>
+                  <div className="d-flex align-items-center justify-content-center row-3 px-3">
+                    <div className="text-center">
+                      <img src={require("../../img/tide_icon.png")}
+                        className="headerTideIcon"
+                      alt="Tide Icon" />
+                    </div>
+                  </div>
+                  <div className="row-3 px-3 weatherWidgetTempFont d-flex align-items-center justify-content-center">
+                    {/* {liveAirTemp} &deg;F */}
+                    2.0 ft
+                  </div>
+                </div>
+              <div className="col-6">
+                56th Street:
+              </div>
+            </div>
+            <div className="d-flex w-100 justify-content-around mt-3">
+              <div className="d-flex col-8">
+                <div className="row-3">
+                </div>
+                  <div className="d-flex align-items-center justify-content-center row-3 px-2">
+                    <MaterialSymbol icon="keyboard_double_arrow_up" size={60} fill grade={-25} color='black' />
+                  </div>
+                  <div className="row-3 weatherWidgetTideFont d-flex align-items-center justify-content-center">
+                    {/* {liveAirTemp} &deg;F */}
+                    3.1 High at 3:49pm
+                  </div>
+                </div>
+              <div className="col-6">
+                River Jetties:
+              </div>
+            </div>
+
           </div>
             
         </div>
