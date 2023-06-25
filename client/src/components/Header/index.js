ccfb5d44a05446cdf98dee098a1f4344efe99870
Clayton S <desertcow07@gmail.com>
2023-06-25 12:17:37 -0700

    Home link patch

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -29,7 +29,7 @@ const Header = () => {
 
   const handleLogoClick = async (event) => {
   event.preventDefault();
-  navigate("/");
+  navigate("/home");
   // console.log("BC Logo Clicked");
   };
 
