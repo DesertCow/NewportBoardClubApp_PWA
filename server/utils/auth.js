efcebc62418ad80e79d01d99710e512cddc5809e
Clayton S <desertcow07@gmail.com>
2023-07-15 19:11:44 -0700

    Updated Auth to take from ENV file

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,12 +1,10 @@
+
 const jwt = require('jsonwebtoken');
 
-//* Grab Key From ENV File
-const secret = process.env.JWT_SECRET;
-const expiration = '2h';
 
 module.exports = {
   signToken: function ({ memberEmail, memberFirstName,memberLastName, _id }) {
     const payload = { memberEmail, memberFirstName, memberLastName, _id };
-    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+    return jwt.sign({ data: payload }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_TOKEN_VALID_LENGTH });
   },
 };
