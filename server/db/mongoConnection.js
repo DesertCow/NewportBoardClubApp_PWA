cc750ffd5ac31bb97754939ca6105d9cf71d8fce
Clayton S <desertcow07@gmail.com>
2023-08-05 20:56:14 -0700

    Cleaned up server page

--- server/db/mongoConnection.js
+++ server/db/mongoConnection.js
@@ -4,8 +4,6 @@ require('dotenv').config();
 
 process.env.MONGODB_URI || 'mongodb://192.168.25.10:28031/',
 
-console.log(process.env.MONGDB_URI);
-
 mongoose.connect(
   process.env.MONGDB_URI,
   {
