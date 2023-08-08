38f9cfdf764dfd075d7b1072e6d7ec58c5426ebb
Clayton S <desertcow07@gmail.com>
2023-08-08 10:36:29 -0700

    Removed server

--- client/src/index.js
+++ client/src/index.js
@@ -7,34 +7,21 @@ import "./reset.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
+import { createRoot } from 'react-dom/client';
 
 import { LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 
-// GraphQL Server URL
+//? GraphQL Server URL
 const client = new ApolloClient({
   // uri: 'http://192.168.25.22:4001',
   uri: 'https://boardclubapp-api.up.railway.app',
-  // uri: localHost,
   cache: new InMemoryCache(),
 });
-
-// import { BrowserRouter as Router } from 'react-router-dom';
-import { createRoot } from 'react-dom/client';
-
-// * ~~ Toastify ~~
-// import { ToastContainer } from "react-toastify";
-
-// * ~~ Cart Wrapper ~~
-// import { CartProvider } from "react-use-cart";
 
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
-
-// const express = require('express');
-// const path = require('path');
-// const { auth } = require('express-openid-connect');
 
 root.render(
   <div className="masterFrameWidth">
